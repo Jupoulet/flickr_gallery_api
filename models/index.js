20809a09e0f276bedcfa32487510affad582cc57
Julien Picard <jup@mbp-de-julien.home>
2021-03-21 18:45:14 +0100

    wip

--- models/index.js
+++ models/index.js
@@ -8,10 +8,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-console.log('Config', config)
-
 let sequelize;
 if (config.use_env_variable) {
+  console.log('PRODUCTION CONFIG', {
+    'process.env': process.env[config.use_env_variable],
+    config,
+  })
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
